2ee6c7efa0878e27229170cb021fa2fefa67d60f
Edward Gao <egao@sent.com>
2016-11-22 02:33:58 -1000

    returns public functions and passes 2 tests

--- vault.js
+++ vault.js
@@ -1,4 +1,15 @@
 'use strict';
 module.exports = function() {
-  
+  function _setValue(newKey, newValue) {
+
+  }
+
+  function _getValue(keyName) {
+    return null;
+  }
+
+  return {
+    setValue: _setValue,
+    getValue: _getValue
+  };
 };

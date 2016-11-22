f0322f8e57d9e41f10c89c1663129a25da853e76
Edward Gao <egao@sent.com>
2016-11-22 02:41:20 -1000

    added _mapping(item) function from iBasics

--- vault.js
+++ vault.js
@@ -14,6 +14,22 @@ module.exports = function() {
     return _mapping(keyName).from(keys).to(values);
   }
 
+  // mapping arrays
+  function _mapping(item) {
+    function _from(arrayOfOrigin) {
+      function _to(arrayOfDestination) {
+        for(var i = 0; i < arrayOfOrigin.length; i++) {
+          if(arrayOfOrigin[i] === item) {
+            return arrayOfDestination[i];
+          }
+        }
+        return null;
+      }
+      return { to: _to };
+    }
+    return { from: _from };
+  }
+
   return {
     setValue: _setValue,
     getValue: _getValue
